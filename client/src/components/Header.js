ad1182e0fd410a5694e828b5043524b54767b238
Tufahel Ahmed <tufahel96@gmail.com>
2023-08-25 01:06:17 +0600

    Refactor code

---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <a href="" className="logo">
+        My logo
+      </a>
+      <nav>
+        <a href="/login">Login</a>
+        <a href="">Register</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
