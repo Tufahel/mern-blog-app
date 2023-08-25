bd966be91d2aa5e5998cd8ae204ca430ec2e9639
Tufahel <tufahel96@gmail.com>
2023-08-25 20:35:47 +0600

    Login setup with jwtoken, bcrypt and cookie

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
+const secret= 'dsfajsr4343jkasj343sdfaskj'
 
-app.use(cors());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://mern-blog-app:883243567@cluster0.5xiwisl.mongodb.net/?retryWrites=true&w=majority')
@@ -22,6 +24,21 @@ app.post('/register', async (req,res) => {
         res.json(userDoc);
     } catch (e) {
         res.status(400).json(e);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        jwt.sign({username, id: userDoc._id}, secret, {}, (err, token) => {
+            if(err) throw err;
+            res.cookie('token', token).json('ok');    
+        })
+        // res.json();
+    } else {
+        res.status(400).json('wrong credentials');
     }
 })
 
