031a64fbf918d6d2d33bae2e2971213c2f8d2358
Tufahel <tufahel96@gmail.com>
2023-08-26 23:42:01 +0600

    Create new post form with posting to database

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const Post = require('./models/Post');
 const bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
+const multer = require('multer');
+const fs = require('fs-extra');
+const uploadMiddleware = multer({dest: 'uploads/'})
+
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
@@ -59,6 +64,23 @@ app.post('/logout', (req,res) => {
     res.cookie('token', '').json('ok');
 })
 
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length-1];
+    const newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+
+    const {title,summary,content} = req.body;
+    const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover: newPath,
+    })
+    res.json({postDoc});
+})
 // mongodb+srv://mern-blog-app:883243567@cluster0.5xiwisl.mongodb.net/?retryWrites=true&w=majority
 
 app.listen(4000);
